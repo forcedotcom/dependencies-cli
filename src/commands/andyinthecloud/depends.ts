55fb674b3317eb7209d5780386782ee267ee80d0
Justin Kalloor <jkalloor@jkalloor-ltm.internal.salesforce.com>
2018-06-13 08:46:26 -0600

    Made modification to show parent Object on any Custom Field node. Furthermore, changed syntax form .then() to await to make code clearer

--- src/commands/andyinthecloud/depends.ts
+++ src/commands/andyinthecloud/depends.ts
@@ -1,5 +1,6 @@
 import {join} from 'path';
 import {core, flags, SfdxCommand} from '@salesforce/command';
+import { listenerCount } from 'cluster';
 
 core.Messages.importMessagesDirectory(join(__dirname, '..', '..', '..'));
 
@@ -12,7 +13,11 @@ SELECT MetadataComponentId, MetadataComponentName, MetadataComponentType, RefMet
 const criteriaSOQL = `
 SELECT MetadataComponentId, MetadataComponentName, MetadataComponentType, RefMetadataComponentId, RefMetadataComponentName, RefMetadataComponentType FROM MetadataComponentDependency Where ${critera}`;
 const filterSOQL = `
-SELECT MetadataComponentId, MetadataComponentName, MetadataComponentType, RefMetadataComponentId, RefMetadataComponentName, RefMetadataComponentType FROM MetadataComponentDependency WHERE RefMetadataComponentName = '${filtername}' AND RefMetadataComponentType = '${filtertype}' AND (NOT MetadataComponentName LIKE '%Test')`;
+SELECT  MetadataComponentId, MetadataComponentName, MetadataComponentType, RefMetadataComponentId, RefMetadataComponentName, RefMetadataComponentType FROM MetadataComponentDependency WHERE RefMetadataComponentName = '${filtername}' AND RefMetadataComponentType = '${filtertype}' AND (NOT MetadataComponentName LIKE '%Test')`;
+const parentSOQL = `SELECT Id,TableEnumOrId FROM CustomField c WHERE c.Id In `;
+const customObjectParentSOQL = `SELECT Id,DeveloperName FROM CustomObject c WHERE c.Id In `;
+const customFieldSOQL = `SELECT MetadataComponentId FROM MetadataComponentDependency WHERE MetadataComponentType = 'CustomField'`;
+const refCustomFieldSOQL = `SELECT RefMetadataComponentId FROM MetadataComponentDependency WHERE RefMetadataComponentType = 'CustomField'`;
 export default class Org extends SfdxCommand {
 
   public static description = '';
@@ -30,8 +35,8 @@ export default class Org extends SfdxCommand {
 
   protected static flagsConfig = {
     // flag with a value (-n, --name=VALUE)
-    name:                   flags.string({char: 'n', description: 'description is a string'}),
-    force:                  flags.boolean({char: 'f', description: 'description is another string'}),
+    name:                   flags.string({char: 'n', description: 'name'}),
+    force:                  flags.boolean({char: 'f', description: 'force'}),
     resultformat:           flags.string({char: 'r', description: 'dot graph format'}),
     metadatacomponentnames: flags.string({char: 'm', description: 'filter by components, e.g. CustomField.Thumbnail'}),
     querycriteria:          flags.string({char: 'q', description: 'query criteria'})
@@ -51,59 +56,94 @@ export default class Org extends SfdxCommand {
     this.collectFlags();
 
     const conn: any = this.org.getConnection();
-
-    var ux = this.ux;
     conn.version = '43.0';
-    conn.tooling.query(
-      critera != null ?
-        criteriaSOQL : // Criteria?
-      filtername != null && filtertype != null ?
-        filterSOQL : // Ref filter?
-        fullSOQL // Full org
-    ).then(function(res) {
-        // Construct the nodes and edge list
-        const nodesMap = new Map();
-        const nodes = [];
-        const edges = [];
-        for (const recordIdx in res.records) {
-          if (recordIdx) {
-            const record = res.records[recordIdx];
-            if(record.RefMetadataComponentName.startsWith('0')) {
-              continue;
+
+    //Get all Custom Field Ids in MetadataComponent and RefMetadata Component
+    const queryResult = await conn.tooling.query(customFieldSOQL);
+    const queryResultRef = await conn.tooling.query(refCustomFieldSOQL);
+    let ids = queryResult.records.map(r => r.MetadataComponentId);
+    let idsRef = queryResultRef.records.map(r => r.RefMetadataComponentId);
+
+    //Concat both lists of ids
+    const idsTotal = ids.concat(idsRef);
+
+    //Find parents by using TableEnumOrId class on list of ids
+    const parents = await conn.tooling.query(parentSOQL + `('${idsTotal.join('\',\'')}')`);
+    const parentRecords = parents.records;
+    
+    //Filter Ids that start with 0
+    let customObjectIdRecords = parentRecords.filter(x => x.TableEnumOrId.startsWith('0'));
+    let customObjectIds = customObjectIdRecords.map(r => r.TableEnumOrId);
+    
+    //Another query to get Custom Object Names from Id
+    const customObjects = await conn.tooling.query(customObjectParentSOQL + `('${customObjectIds.join('\',\'')}')`);
+    const customObjectRecords = customObjects.records;
+    
+    var ux = this.ux;
+
+    try {
+      const res = await conn.tooling.query(
+          critera != null ? 
+          criteriaSOQL : // Criteria?
+          filtername != null && filtertype != null ?
+          filterSOQL : // Ref filter?
+          fullSOQL // Full org
+          );   
+      const nodesMap = new Map();
+      const nodes = [];
+      const edges = [];
+      for (const recordIdx in res.records) {
+        let parentName = '';
+        if (recordIdx) {
+          const record = res.records[recordIdx];
+          if(record.RefMetadataComponentName.startsWith('0')) {
+            continue;
+          }
+          //Get parent for Custom Field if possible
+          let parent = parentRecords.filter(x => x.Id == record.MetadataComponentId || x.Id == record.RefMetadataComponentId);
+          if (parent.length > 0) {
+            parentName = parent[0].TableEnumOrId;
+            if (parentName.startsWith('0')) {
+              //Look up in Custom Objects Table
+              parent = customObjectRecords.filter(x => x.Id == parentName);
+              parentName = parent[0].DeveloperName;
             }
-            nodesMap.set(record.MetadataComponentId, { name: record.MetadataComponentName, type: record.MetadataComponentType });
-            nodesMap.set(record.RefMetadataComponentId, { name: record.RefMetadataComponentName, type: record.RefMetadataComponentType });
-            edges.push({ from : record.MetadataComponentId, to: record.RefMetadataComponentId });
-          }
-        }
-        for (const [key, value] of nodesMap) {
-          nodes.push({ id : key, node : value});
-        }
-        // Render as DOT format
-        ux.log('digraph graphname {');
-        ux.log('  rankdir=RL;');
-        ux.log('  node[shape=Mrecord, bgcolor=black, fillcolor=lightblue, style=filled];');
-        ux.log('  // Nodes');
 
-        for (const nodeIdx in nodes) {
-          if (nodeIdx) {
-            ux.log('  X' + nodes[nodeIdx].id + ' [label=<' + nodes[nodeIdx].node.name + '<BR/><FONT POINT-SIZE="8">' + nodes[nodeIdx].node.type + '</FONT>>]');
+            parentName = parentName + '.';
           }
+          nodesMap.set(record.MetadataComponentId, { parent: parentName, name: record.MetadataComponentName, type: record.MetadataComponentType });
+          nodesMap.set(record.RefMetadataComponentId, { parent: parentName, name: record.RefMetadataComponentName, type: record.RefMetadataComponentType });
+          edges.push({ from : record.MetadataComponentId, to: record.RefMetadataComponentId });
         }
-
-        ux.log('  // Paths');
-        for (const edgeIdx in edges) {
-          if (edgeIdx) {
-            ux.log('  X' + edges[edgeIdx].from + '->X' + edges[edgeIdx].to);
-          }
-        }
-
-        ux.log('}');
       }
-    ).catch(function(error) {
-        ux.error(error);
+      for (const [key, value] of nodesMap) {
+        nodes.push({ id : key, node : value});
       }
-    );
+      // Render as DOT format
+      ux.log('digraph graphname {');
+      ux.log('  rankdir=RL;');
+      ux.log('  node[shape=Mrecord, bgcolor=black, fillcolor=lightblue, style=filled];');
+      ux.log('  // Nodes');
+
+      for (const nodeIdx in nodes) {
+        if (nodeIdx) {
+          ux.log('  X' + nodes[nodeIdx].id + ' [label=<' + nodes[nodeIdx].node.parent + nodes[nodeIdx].node.name + '<BR/><FONT POINT-SIZE="8">' + nodes[nodeIdx].node.type + '</FONT>>]');
+        }
+      }
+
+      ux.log('  // Paths');
+      for (const edgeIdx in edges) {
+        if (edgeIdx) {
+          ux.log('  X' + edges[edgeIdx].from + '->X' + edges[edgeIdx].to);
+        }
+      }
+
+      ux.log('}');                     
+    } catch (error) {
+      ux.error(error);
+    }
+
+
   }
 
   private collectFlags(): any {
