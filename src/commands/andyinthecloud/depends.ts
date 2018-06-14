2ffc01014dd26e11ff440f09ea6378512e497875
Thomas <t.dvornik@salesforce.com>
2018-06-14 10:06:50 -0600

    Get padding tests and refactor for more readable code

--- src/commands/andyinthecloud/depends.ts
+++ src/commands/andyinthecloud/depends.ts
@@ -1,192 +1,53 @@
-import {join} from 'path';
-import {core, flags, SfdxCommand} from '@salesforce/command';
-import { listenerCount } from 'cluster';
+import { core, flags, SfdxCommand } from '@salesforce/command';
+import { DependencyGraph, MetadataComponentDependency } from '../../lib/dependencyGraph';
 
-core.Messages.importMessagesDirectory(join(__dirname, '..', '..', '..'));
+core.Messages.importMessagesDirectory(__dirname);
 
-let critera: string;
-let filtername: string;
-let filtertype: string;
+const messages = core.Messages.loadMessages('dependencies-cli', 'depends');
 
-const fullSOQL = `
-SELECT MetadataComponentId, MetadataComponentName, MetadataComponentType, RefMetadataComponentId, RefMetadataComponentName, RefMetadataComponentType FROM MetadataComponentDependency`;
-const criteriaSOQL = `
-SELECT MetadataComponentId, MetadataComponentName, MetadataComponentType, RefMetadataComponentId, RefMetadataComponentName, RefMetadataComponentType FROM MetadataComponentDependency Where ${critera}`;
-const filterSOQL = `
-SELECT  MetadataComponentId, MetadataComponentName, MetadataComponentType, RefMetadataComponentId, RefMetadataComponentName, RefMetadataComponentType FROM MetadataComponentDependency WHERE RefMetadataComponentName = '${filtername}' AND RefMetadataComponentType = '${filtertype}' AND (NOT MetadataComponentName LIKE '%Test')`;
-const fieldObjectSOQL = `SELECT Id,TableEnumOrId FROM CustomField c WHERE c.Id In `;
-const vruleObjectSOQL = `SELECT Id,EntityDefinitionId FROM ValidationRule c WHERE c.Id In `;
-const customObjectParentSOQL = `SELECT Id,DeveloperName FROM CustomObject c WHERE c.Id In `;
-const customComponentsSOQL = `SELECT MetadataComponentId,RefMetadataComponentId FROM MetadataComponentDependency WHERE (MetadataComponentType = 'CustomField' OR RefMetadataComponentType = 'CustomField') OR (MetadataComponentType = 'ValidationRule' OR RefMetadataComponentType = 'ValidationRule')`;
 export default class Org extends SfdxCommand {
-
-  public static description = '';
-
-  public static examples = [
-  `$ sfdx hello:org --targetusername myOrg@example.com --targetdevhubusername devhub@org.com
-  Hello world! Your org id is: 00Dxx000000004321 and your hub org id is: 00Dxx0000000001234
-  `,
-  `$ sfdx hello:org --name myname --targetusername myOrg@example.com
-  Hello myname! Your org id is: 00Dxx000000004321
-  `
-  ];
-
-  public static args = [{name: 'file'}];
+  public static description = messages.getMessage('description');
+  public static examples = [messages.getMessage('example1')];
 
   protected static flagsConfig = {
-    // flag with a value (-n, --name=VALUE)
-    name:                   flags.string({char: 'n', description: 'name'}),
-    force:                  flags.boolean({char: 'f', description: 'force'}),
-    resultformat:           flags.string({char: 'r', description: 'dot graph format'}),
-    metadatacomponentnames: flags.string({char: 'm', description: 'filter by components, e.g. CustomField.Thumbnail'}),
-    querycriteria:          flags.string({char: 'q', description: 'query criteria'})
+    resultformat: flags.string({ char: 'r', description: messages.getMessage('resultformatFlagDescription'), default: 'dot', options: ['dot'] }),
+    metadatacomponentname: flags.string({ char: 'm', description: messages.getMessage('metadatacomponentnameFlagDescription') }),
+    querycriteria: flags.string({ char: 'q', description: messages.getMessage('querycriteriaFlagDescription') })
   };
 
-  // Comment this out if your command does not require an org username
   protected static requiresUsername = true;
 
-  // Comment this out if your command does not support a hub org username
-  protected static supportsDevhubUsername = true;
-
-  // Set this to true if your command requires a project workspace; 'requiresProject' is false by default
-  protected static requiresProject = false;
-9960590757574390
-  public async run(): Promise<any> {
-    
-    this.collectFlags();
-
-    const conn: any = this.org.getConnection();
+  public async run(): Promise<core.AnyJson> {
+    const conn = this.org.getConnection();
     conn.version = '43.0';
 
-    //Get all Custom Field Ids in MetadataComponent and RefMetadata Component
-    const customComponentIds = await conn.tooling.query(customComponentsSOQL);
+    const deps = new DependencyGraph(conn.tooling);
+    await deps.init(await this.getDependencyRecords());
 
-    let ids = customComponentIds.records.map(r => r.MetadataComponentId);
-    let idsRef = customComponentIds.records.map(r => r.RefMetadataComponentId);
-
-    //Concat both lists of ids
-    var idsTotal = ids.concat(idsRef);
-    var idSet = new Set(idsTotal); //remove duplicates
-    idsTotal = Array.from(idSet);
-
-    //Find object for Custom Fields by using TableEnumOrId class on list of ids
-    const fieldObjects = await conn.tooling.query(fieldObjectSOQL + `('${idsTotal.join('\',\'')}')`);
-    const fieldObjectRecords = fieldObjects.records;
-
-    //Find object for ValidationRules using same method
-    const vruleObjects = await conn.tooling.query(vruleObjectSOQL + `('${idsTotal.join('\',\'')}')`);
-    const vruleObjectRecords = vruleObjects.records;
-
-
-    //Filter Ids that start with 0
-    let fieldObjectIdRecords = fieldObjectRecords.filter(x => x.TableEnumOrId.startsWith('0'));
-    let customObjectIds = fieldObjectIdRecords.map(r => r.TableEnumOrId);
-
-    //Filter Ids that start with 0 from vrule
-    let vruleObjectIdRecords = vruleObjectRecords.filter(x => x.EntityDefinitionId.startsWith('0'));
-    //Add to list
-    customObjectIds = customObjectIds.concat(vruleObjectIdRecords.map(r => r.EntityDefinitionId));
-    
-    //Another query to get Custom Object Names from Id
-    const customObjects = await conn.tooling.query(customObjectParentSOQL + `('${customObjectIds.join('\',\'')}')`);
-    const customObjectRecords = customObjects.records;
-
-    //Put all info into   a Map
-    let parentRecords = new Map();
-
-    for (const recordIdx in fieldObjectRecords) {
-      let parentRecord = fieldObjectRecords[recordIdx];
-      let val = parentRecord.TableEnumOrId;
-      if (val.startsWith('0')) {
-        //Custom Object
-        let customObject= customObjectRecords.filter(x => x.Id.startsWith(val));
-        val = customObject[0].DeveloperName;
-      }
-      parentRecords.set(parentRecord.Id, val);
+    // Only dot format is allowed by the flags property, but put
+    // this check in case you add more later
+    if (this.flags.resultformat === 'dot') {
+      this.ux.log(deps.toDotFormat());
     }
 
-    for (const recordIdx in vruleObjectRecords) {
-      let parentRecord = vruleObjectRecords[recordIdx];
-      let val = parentRecord.EntityDefinitionId;
-      if (val.startsWith('0')) {
-        //Custom Object
-        let customObject= customObjectRecords.filter(x => x.Id.startsWith(val));
-        val = customObject[0].DeveloperName;
-      }
-      parentRecords.set(parentRecord.Id, val);
-    }
-
-    
-    var ux = this.ux;
-
-    try {
-      const res = await conn.tooling.query(
-          critera != null ? 
-          criteriaSOQL : // Criteria?
-          filtername != null && filtertype != null ?
-          filterSOQL : // Ref filter?
-          fullSOQL // Full org
-          );   
-      const nodesMap = new Map();
-      const nodes = [];
-      const edges = [];
-      for (const recordIdx in res.records) {
-        let parentName = '';
-        let refParentName = '';
-        if (recordIdx) {
-          const record = res.records[recordIdx];
-          if(record.RefMetadataComponentName.startsWith('0')) {
-            continue;
-          }
-          if (record.MetadataComponentType == "CustomField" || record.MetadataComponentType == "ValidationRule") {
-            parentName = parentRecords.get(record.MetadataComponentId) + ".";
-          }
-
-          if (record.RefMetadataComponentType == "CustomField" || record.RefMetadataComponentType == "ValidationRule") {
-            refParentName = parentRecords.get(record.RefMetadataComponentId) + ".";
-          }
-          nodesMap.set(record.MetadataComponentId, { parent: parentName, name: record.MetadataComponentName, type: record.MetadataComponentType });
-          nodesMap.set(record.RefMetadataComponentId, { parent: refParentName, name: record.RefMetadataComponentName, type: record.RefMetadataComponentType });
-          edges.push({ from : record.MetadataComponentId, to: record.RefMetadataComponentId });
-        }
-      }
-      for (const [key, value] of nodesMap) {
-        nodes.push({ id : key, node : value});
-      }
-      // Render as DOT format
-      ux.log('digraph graphname {');
-      ux.log('  rankdir=RL;');
-      ux.log('  node[shape=Mrecord, bgcolor=black, fillcolor=lightblue, style=filled];');
-      ux.log('  // Nodes');
-
-      for (const nodeIdx in nodes) {
-        if (nodeIdx) {
-          ux.log('  X' + nodes[nodeIdx].id + ' [label=<' + nodes[nodeIdx].node.parent + nodes[nodeIdx].node.name + '<BR/><FONT POINT-SIZE="8">' + nodes[nodeIdx].node.type + '</FONT>>]');
-        }
-      }
-
-      ux.log('  // Paths');
-      for (const edgeIdx in edges) {
-        if (edgeIdx) {
-          ux.log('  X' + edges[edgeIdx].from + '->X' + edges[edgeIdx].to);
-        }
-      }
-
-      ux.log('}');                     
-    } catch (error) {
-      ux.error(error);
-    }
-
-
+    // All commands should support --json
+    return deps.toJson();
   }
 
-  private collectFlags(): any {
-    const orgId = this.org.getOrgId();
-    const filter = this.flags.metadatacomponentnames;
-    critera = this.flags.querycriteria;
-    filtername = filter != null ? filter.split('.')[1] : null;
-    filtertype = filter != null ? filter.split('.')[0] : null;
-    return this.org.getConnection();
-  }
+  private async getDependencyRecords() {
+    const query = this.org.getConnection().tooling.query<MetadataComponentDependency>('SELECT MetadataComponentId, MetadataComponentName, MetadataComponentType, RefMetadataComponentId, RefMetadataComponentName, RefMetadataComponentType FROM MetadataComponentDependency');
 
+    if (this.flags.querycriteria) {
+      query.where(this.flags.querycriteria);
+    }
+
+    if (this.flags.metadatacomponentname) {
+      const filter = this.flags.metadatacomponentname;
+      const filtername = filter != null ? filter.split('.')[1] : null;
+      const filtertype = filter != null ? filter.split('.')[0] : null;
+      query.where(`RefMetadataComponentName = '${filtername}' AND RefMetadataComponentType = '${filtertype}' AND (NOT MetadataComponentName LIKE '%Test')`);
+    }
+
+    return (await query).records;
+  }
 }
---
+++ src/lib/dependencyGraph.ts
@@ -0,0 +1,191 @@
+import { Tooling } from 'jsforce';
+
+export type Node = {
+  parent: string;
+  name: string;
+  type: string;
+};
+
+export type Edge = {
+  from: string;
+  to: string;
+};
+
+export interface Record {
+  Id?: string;
+}
+
+export interface MetadataComponentDependency extends Record {
+  MetadataComponentId: string;
+  MetadataComponentName: string;
+  MetadataComponentType: string;
+  RefMetadataComponentId: string;
+  RefMetadataComponentName: string;
+  RefMetadataComponentType: string;
+}
+
+export interface CustomField extends Record {
+  TableEnumOrId: string;
+}
+
+export interface ValidationRule extends Record {
+  EntityDefinitionId: string;
+}
+
+export interface CustomObject extends Record {
+  DeveloperName: string;
+}
+
+export class DependencyGraph {
+  public nodes: Array<{ id: string, node: Node }> = [];
+  public edges: Edge[] = [];
+
+  private allComponentIds: string[];
+  private customFields: CustomField[];
+  private validationRules: ValidationRule[];
+  private customObjects: CustomObject[];
+
+  constructor(private tooling: Tooling) { }
+
+  public async init(records: MetadataComponentDependency[]) {
+    this.allComponentIds = await this.retrieveAllComponentIds();
+    this.customFields = await this.retrieveCustomFields(this.allComponentIds);
+    this.validationRules = await this.retrieveValidationRules(this.allComponentIds);
+    this.customObjects = await this.retrieveCustomObjects(this.getObjectIds());
+
+    const parentRecords = this.getParentRecords();
+    const nodesMap = new Map();
+
+    for (const record of records) {
+      let parentName = '';
+      let refParentName = '';
+
+      if (record.RefMetadataComponentName.startsWith('0')) {
+        continue;
+      }
+      if (record.MetadataComponentType === 'CustomField' || record.MetadataComponentType === 'ValidationRule') {
+        parentName = parentRecords.get(record.MetadataComponentId) + '.';
+      }
+
+      if (record.RefMetadataComponentType === 'CustomField' || record.RefMetadataComponentType === 'ValidationRule') {
+        refParentName = parentRecords.get(record.RefMetadataComponentId) + '.';
+      }
+
+      nodesMap.set(record.MetadataComponentId, { parent: parentName, name: record.MetadataComponentName, type: record.MetadataComponentType });
+      nodesMap.set(record.RefMetadataComponentId, { parent: refParentName, name: record.RefMetadataComponentName, type: record.RefMetadataComponentType });
+      this.edges.push({ from: record.MetadataComponentId, to: record.RefMetadataComponentId });
+
+    }
+    for (const [key, value] of nodesMap) {
+      this.nodes.push({ id: key, node: value });
+    }
+  }
+
+  /**
+   * Render as DOT format
+   */
+  public toDotFormat(): string {
+
+    // TODO Depending on the size of orgs, you may not want to
+    // keep all this in memory. However, you don't want to do
+    // console.log in library code, and this method really belongs
+    // on the graph. Instead of using ux.log on every
+    // line, just return a stream that you continue to write to,
+    // then the command can call ux.log from the stream events.
+
+    let dot = 'digraph graphname {\n';
+    dot += '  rankdir=RL;\n';
+    dot += '  node[shape=Mrecord, bgcolor=black, fillcolor=lightblue, style=filled];\n';
+    dot += '  // Nodes\n';
+
+    for (const node of this.nodes) {
+      dot += `  X${node.id} [label=<${node.node.parent}${node.node.name}<BR/><FONT POINT-SIZE="8">${node.node.type}</FONT>>]\n`;
+    }
+
+    dot += '  // Paths\n';
+    for (const edge of this.edges) {
+      dot += `  X${edge.from}->X${edge.to}\n`;
+    }
+
+    dot += '}';
+    return dot;
+  }
+
+  public toJson() {
+    return { nodes: this.nodes, edges: this.edges };
+  }
+
+  public getParentRecords(): Map<string, string> {
+    // Put all info into a Map
+    const parentRecords = new Map();
+
+    this.populateIdToDeveloperNameMap(parentRecords, this.validationRules, 'EntityDefinitionId');
+    this.populateIdToDeveloperNameMap(parentRecords, this.customFields, 'TableEnumOrId');
+
+    return parentRecords;
+  }
+
+  public async retrieveRecords<T>(query: string) {
+    return (await this.tooling.query<T>(query)).records;
+  }
+
+  public async retrieveCustomFields(ids: string[]): Promise<CustomField[]> {
+    const query = `SELECT Id, TableEnumOrId FROM CustomField c WHERE c.Id In ${this.arrayToInIdString(ids)}`;
+    return await this.retrieveRecords<CustomField>(query);
+  }
+
+  public async retrieveValidationRules(ids: string[]): Promise<ValidationRule[]> {
+    const query = `SELECT Id, EntityDefinitionId FROM ValidationRule c WHERE c.Id In ${this.arrayToInIdString(ids)}`;
+    return await this.retrieveRecords<ValidationRule>(query);
+  }
+
+  public async retrieveCustomObjects(ids: string[]): Promise<CustomObject[]> {
+    const query = `SELECT Id, DeveloperName FROM CustomObject c WHERE c.Id In ${this.arrayToInIdString(this.getObjectIds())}`;
+    return await this.retrieveRecords<CustomObject>(query);
+  }
+
+  private async retrieveAllComponentIds(): Promise<string[]> {
+    const query = "SELECT MetadataComponentId,RefMetadataComponentId FROM MetadataComponentDependency WHERE (MetadataComponentType = 'CustomField' OR RefMetadataComponentType = 'CustomField') OR (MetadataComponentType = 'ValidationRule' OR RefMetadataComponentType = 'ValidationRule')";
+
+    // Get all Custom Field Ids in MetadataComponent and RefMetadata Component
+    const customComponentIds = await this.retrieveRecords<MetadataComponentDependency>(query);
+
+    const componentIds = customComponentIds.map(r => r.MetadataComponentId);
+    const refComponentIds = customComponentIds.map(r => r.RefMetadataComponentId);
+
+    // Concat both lists of ids
+    let ids = componentIds.concat(refComponentIds);
+    // Remove duplicates
+    ids = Array.from(new Set(ids));
+
+    return ids;
+  }
+
+  private getObjectIds() {
+    // Filter Ids that start with 0
+    const fieldObjectIdRecords = this.customFields.filter(x => x.TableEnumOrId.startsWith('0'));
+    // Filter Ids that start with 0 from vrule
+    const vruleObjectIdRecords = this.validationRules.filter(x => x.EntityDefinitionId.startsWith('0'));
+
+    return [
+      ...fieldObjectIdRecords.map(r => r.TableEnumOrId),
+      ...vruleObjectIdRecords.map(r => r.EntityDefinitionId)
+    ];
+  }
+
+  private populateIdToDeveloperNameMap<T>(map: Map<string, string>, records: T[], fieldName: string): void {
+    for (const record of records) {
+      let val = record[fieldName];
+      if (val.startsWith('0')) {
+        // Grab the custom object the field points to
+        const customObject = this.customObjects.filter(x => x.Id.startsWith(val));
+        val = customObject[0].DeveloperName;
+      }
+      map.set(record['Id'], val);
+    }
+  }
+
+  private arrayToInIdString(ids) {
+    return `('${ids.join('\',\'')}')`;
+  }
+}
