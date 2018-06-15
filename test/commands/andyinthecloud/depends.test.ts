957dfc355a13ae60959f997efb2a7e07ecae2daa
Justin Kalloor <jkalloor@jkalloor-ltm.internal.salesforce.com>
2018-06-15 09:47:31 -0600

    Several Tests were added to assure functionality. API requests were mocked and so no connection is needed to run tests.

--- test/commands/andyinthecloud/depends.test.ts
+++ test/commands/andyinthecloud/depends.test.ts
@@ -1,4 +1,83 @@
 import { expect, test } from '@salesforce/command/dist/test';
+import { dotOutput1, dotOutput2, oneObjectRecords, TwoFields1VRRecords, customFieldRecords, validationRuleRecords, customObjects} from '../../lib/dependencyGraphy.test';
+import { DEFAULT_ECDH_CURVE } from 'tls';
+
+const urlGetRecords = 'http://na30.salesforce.com/services/data/v43.0/tooling/query?q=SELECT%20MetadataComponentId%2C%20MetadataComponentName%2C%20MetadataComponentType%2C%20RefMetadataComponentId%2C%20RefMetadataComponentName%2C%20RefMetadataComponentType%20FROM%20MetadataComponentDependency';
+const urlGetAllComponents = "http://na30.salesforce.com/services/data/v43.0/tooling/query?q=SELECT%20MetadataComponentId%2CRefMetadataComponentId%20FROM%20MetadataComponentDependency%20WHERE%20(MetadataComponentType%20%3D%20'CustomField'%20OR%20RefMetadataComponentType%20%3D%20'CustomField')%20OR%20(MetadataComponentType%20%3D%20'ValidationRule'%20OR%20RefMetadataComponentType%20%3D%20'ValidationRule')";
+const urlGetCustomFields = "http://na30.salesforce.com/services/data/v43.0/tooling/query?q=SELECT%20Id%2C%20TableEnumOrId%20FROM%20CustomField%20c%20WHERE%20c.Id%20In";
+const urlGetValidationRules = "http://na30.salesforce.com/services/data/v43.0/tooling/query?q=SELECT%20Id%2C%20EntityDefinitionId%20FROM%20ValidationRule%20c%20WHERE%20c.Id%20In";
+const urlGetCustomObjects= "http://na30.salesforce.com/services/data/v43.0/tooling/query?q=SELECT%20Id%2C%20DeveloperName%20FROM%20CustomObject%20c%20WHERE%20c.Id%20In";
+
+//TWO OBJECT TEST QUERY RETURNS -------------------
+const twoObjectRecords = [{
+  MetadataComponentId: '1',
+  MetadataComponentName: 'Object1',
+  MetadataComponentType: 'Object',
+  RefMetadataComponentId: '2',
+  RefMetadataComponentName: 'Object2',
+  RefMetadataComponentType: 'Object'
+}];
+
+const dotOutput2Objects = 
+`// Nodes
+  X1 [label=<Object1<BR/><FONT POINT-SIZE="8">Object</FONT>>]
+  X2 [label=<Object2<BR/><FONT POINT-SIZE="8">Object</FONT>>]
+  // Paths
+  X1->X2
+}`;
+
+// -----------------------------------------------
+//ONE CUSTOM FIELD TEST QUERY RETURNS ------------
+const oneCustomFieldRecords = [{
+  MetadataComponentId: '1',
+  MetadataComponentName: 'CustomField1',
+  MetadataComponentType: 'CustomField',
+  RefMetadataComponentId: '2',
+  RefMetadataComponentName: 'Object1',
+  RefMetadataComponentType: 'Object'
+}];
+
+const oneCustomComponent = [
+  {MetadataComponentId: '1', RefMetadataComponentId: '2'}
+];
+
+const dotOutput1CustomField = 
+`// Nodes
+  X1 [label=<ObjectA.CustomField1<BR/><FONT POINT-SIZE="8">CustomField</FONT>>]
+  X2 [label=<Object1<BR/><FONT POINT-SIZE="8">Object</FONT>>]
+  // Paths
+  X1->X2
+}`
+
+const oneCustomField = [
+  {Id: '1', TableEnumOrId: 'ObjectA'}
+]
+
+// ------------------------------------------------
+//ONE VALIDATION RULE TEST QUERY RETURNS ----------
+const oneValidationRuleRecords = [{
+  MetadataComponentId: '1',
+  MetadataComponentName: 'ValidationRule1',
+  MetadataComponentType: 'ValidationRule',
+  RefMetadataComponentId: '2',
+  RefMetadataComponentName: 'Object1',
+  RefMetadataComponentType: 'Object'
+}];
+
+const oneValidationRule = [
+  {Id: '1', EntityDefinitionId: 'ObjectA'}
+];
+
+const dotOutput1ValidationRule = 
+`// Nodes
+  X1 [label=<ObjectA.ValidationRule1<BR/><FONT POINT-SIZE="8">ValidationRule</FONT>>]
+  X2 [label=<Object1<BR/><FONT POINT-SIZE="8">Object</FONT>>]
+  // Paths
+  X1->X2
+}`
+
+// ------------------------------------------------
+
 
 describe('org', () => {
   test
@@ -13,8 +92,8 @@ describe('org', () => {
       // rules, etc.
       // const request = args[0];
 
-      // Just return empty everything for this test
-      return { records: [] };
+      // Just return empty everything for this 
+        return { records: [] };
     })
     .stdout({ print: true })
     .command(['andyinthecloud:depends', '--targetusername', 'test@org.com'])
@@ -28,5 +107,173 @@ describe('org', () => {
 }
 `;
       expect(ctx.stdout).to.equal(expectedEmptyOuput);
-    });
+    })
+});
+
+
+describe('org test one object', () => {
+  test
+    // Mock an org that the command can use
+    .withOrg({ username: 'test@org.com' }, true)
+    .withConnectionRequest(async (...args) => {
+      // You can set a debugger here, and inspect request
+      // There are things on here you can use to determine
+      // what data to return, like the url path. So in another
+      // test, you may have several different ifs returning
+      // data for custom fields, custom objects, validation
+      // rules, etc.
+      // const request = args[0];
+
+        if (args[0].url.startsWith(urlGetRecords)) {
+          //very first request, looking for records
+          return {records: oneObjectRecords}
+        }
+        return {records: []};
+    })
+    .stdout({ print: true })
+    .command(['andyinthecloud:depends', '--targetusername', 'test@org.com'])
+    .it('runs org --targetusername test@org.com', ctx => {
+    
+      expect(ctx.stdout).to.contains(dotOutput1);
+    })
+});
+
+describe('org test two objects' , () => {
+  const components = {
+  };
+  test
+    // Mock an org that the command can use
+    .withOrg({ username: 'test@org.com' }, true)
+    .withConnectionRequest(async (...args) => {
+      // You can set a debugger here, and inspect request
+      // There are things on here you can use to determine
+      // what data to return, like the url path. So in another
+      // test, you may have several different ifs returning
+      // data for custom fields, custom objects, validation
+      // rules, etc.
+      // const request = args[0];
+
+        if (args[0].url.startsWith(urlGetRecords)) {
+          //very first request, looking for records
+          return {records: twoObjectRecords}
+        }
+        return {records: []};
+    })
+    .stdout({ print: true })
+    .command(['andyinthecloud:depends', '--targetusername', 'test@org.com'])
+    .it('runs org --targetusername test@org.com', ctx => {
+    
+      expect(ctx.stdout).to.contains(dotOutput2Objects);
+    })
+});
+
+describe('org test one custom field' , () => {
+  const components = {
+  };
+  test
+    // Mock an org that the command can use
+    .withOrg({ username: 'test@org.com' }, true)
+    .withConnectionRequest(async (...args) => {
+      // You can set a debugger here, and inspect request
+      // There are things on here you can use to determine
+      // what data to return, like the url path. So in another
+      // test, you may have several different ifs returning
+      // data for custom fields, custom objects, validation
+      // rules, etc.
+      // const request = args[0];
+
+        if (args[0].url.startsWith(urlGetRecords)) {
+          //very first request, looking for records
+          return {records: oneCustomFieldRecords}
+        } else if (args[0].url.startsWith(urlGetAllComponents)) {
+          return {records: oneCustomComponent};
+        } else if (args[0].url.startsWith(urlGetCustomFields)) {
+          return {records: oneCustomField};
+        } else if (args[0].url.startsWith(urlGetValidationRules)) {
+          return {records: []};
+        } else if (args[0].url.startsWith(urlGetCustomObjects)) {
+          return {records: []};
+        }
+        return {records: []};
+    })
+    .stdout({ print: true })
+    .command(['andyinthecloud:depends', '--targetusername', 'test@org.com'])
+    .it('runs org --targetusername test@org.com', ctx => {
+    
+      expect(ctx.stdout).to.contains(dotOutput1CustomField);
+    })
+});
+
+describe('org test one validation rule' , () => {
+  const components = {
+  };
+  test
+    // Mock an org that the command can use
+    .withOrg({ username: 'test@org.com' }, true)
+    .withConnectionRequest(async (...args) => {
+      // You can set a debugger here, and inspect request
+      // There are things on here you can use to determine
+      // what data to return, like the url path. So in another
+      // test, you may have several different ifs returning
+      // data for custom fields, custom objects, validation
+      // rules, etc.
+      // const request = args[0];
+
+        if (args[0].url.startsWith(urlGetRecords)) {
+          //very first request, looking for records
+          return {records: oneValidationRuleRecords}
+        } else if (args[0].url.startsWith(urlGetAllComponents)) {
+          return {records: oneCustomComponent};
+        } else if (args[0].url.startsWith(urlGetCustomFields)) {
+          return {records: []};
+        } else if (args[0].url.startsWith(urlGetValidationRules)) {
+          return {records: oneValidationRule};
+        } else if (args[0].url.startsWith(urlGetCustomObjects)) {
+          return {records: []};
+        }
+        return {records: []};
+    })
+    .stdout({ print: true })
+    .command(['andyinthecloud:depends', '--targetusername', 'test@org.com'])
+    .it('runs org --targetusername test@org.com', ctx => {
+    
+      expect(ctx.stdout).to.contains(dotOutput1ValidationRule);
+    })
+});
+
+describe('org test two custom fields, 1 validation rule' , () => {
+  const components = {
+  };
+  test
+    // Mock an org that the command can use
+    .withOrg({ username: 'test@org.com' }, true)
+    .withConnectionRequest(async (...args) => {
+      // You can set a debugger here, and inspect request
+      // There are things on here you can use to determine
+      // what data to return, like the url path. So in another
+      // test, you may have several different ifs returning
+      // data for custom fields, custom objects, validation
+      // rules, etc.
+      // const request = args[0];
+
+        if (args[0].url.startsWith(urlGetRecords)) {
+          //very first request, looking for records
+          return {records: TwoFields1VRRecords}
+        } else if (args[0].url.startsWith(urlGetAllComponents)) {
+          return {records: []};
+        } else if (args[0].url.startsWith(urlGetCustomFields)) {
+          return {records: customFieldRecords};
+        } else if (args[0].url.startsWith(urlGetValidationRules)) {
+          return {records: validationRuleRecords};
+        } else if (args[0].url.startsWith(urlGetCustomObjects)) {
+          return {records: customObjects};
+        }
+        return {records: []};
+    })
+    .stdout({ print: true })
+    .command(['andyinthecloud:depends', '--targetusername', 'test@org.com'])
+    .it('runs org --targetusername test@org.com', ctx => {
+    
+      expect(ctx.stdout).to.contains(dotOutput2);
+    })
 });
--- test/lib/dependencyGraphy.test.ts
+++ test/lib/dependencyGraphy.test.ts
@@ -2,19 +2,69 @@ import { expect } from 'chai';
 import { stub } from 'sinon';
 import { DependencyGraph, MetadataComponentDependency } from '../../src/lib/dependencyGraph';
 
-describe('dependency graph', () => {
+
+export const oneObjectRecords: MetadataComponentDependency[] = [{
+  MetadataComponentId: '1',
+  MetadataComponentName: 'Object1',
+  MetadataComponentType: 'Object',
+  RefMetadataComponentId: '1',
+  RefMetadataComponentName: 'Object1',
+  RefMetadataComponentType: 'Object'
+}];
+
+export const dotOutput1 = 
+` // Nodes
+  X1 [label=<Object1<BR/><FONT POINT-SIZE="8">Object</FONT>>]
+  // Paths
+  X1->X1
+}`;
+
+  //Test Both Custom and Standard Objects
+  export const customFieldRecords = [
+    {Id: '1', TableEnumOrId: 'ObjectA'},
+    {Id: '3', TableEnumOrId: '0CustomObject'}
+  ];
+
+  export const validationRuleRecords = [
+    {Id: '2', EntityDefinitionId: 'ObjectA'}
+  ]
+
+  export const customObjects = [
+    {Id: '0CustomObject', DeveloperName: 'CustomObjectA'}
+  ];
+
+  // 3 objects, CustomField2 -> CustomField 1 -> ValidationRule 1
+export const TwoFields1VRRecords: MetadataComponentDependency[] = [{
+    MetadataComponentId: '1',
+    MetadataComponentName: 'CustomField1',
+    MetadataComponentType: 'CustomField',
+    RefMetadataComponentId: '2',
+    RefMetadataComponentName: 'ValidationRule1',
+    RefMetadataComponentType: 'ValidationRule'
+  }, {
+    MetadataComponentId: '3',
+    MetadataComponentName: 'CustomField2',
+    MetadataComponentType: 'CustomField',
+    RefMetadataComponentId: '1',
+    RefMetadataComponentName: 'CustomField1',
+    RefMetadataComponentType: 'CustomField'
+  }];
+
+export const dotOutput2 = 
+  `// Nodes
+  X1 [label=<ObjectA.CustomField1<BR/><FONT POINT-SIZE="8">CustomField</FONT>>]
+  X2 [label=<ObjectA.ValidationRule1<BR/><FONT POINT-SIZE="8">ValidationRule</FONT>>]
+  X3 [label=<CustomObjectA.CustomField2<BR/><FONT POINT-SIZE="8">CustomField</FONT>>]
+  // Paths
+  X1->X2
+  X3->X1
+}`;
+
+describe('no relationship single node dependency graph', () => {
   let graph: DependencyGraph;
 
   // Can define here if you want to use them in multiple tests
   // TODO you probably want to make this more meaningful
-  const records: MetadataComponentDependency[] = [{
-    MetadataComponentId: 'blah',
-    MetadataComponentName: 'blah',
-    MetadataComponentType: 'blah',
-    RefMetadataComponentId: 'blah',
-    RefMetadataComponentName: 'blah',
-    RefMetadataComponentType: 'blah'
-  }];
 
   beforeEach(() => {
     // Don't use a connection, just mock methods
@@ -29,10 +79,97 @@ describe('dependency graph', () => {
   // they should be covered from the public method tests.
   describe('init', () => {
     it('populates nodes and edges for one record', async () => {
-      await graph.init(records);
+      await graph.init(oneObjectRecords);
 
       expect(graph.nodes.length).to.equal(1);
       expect(graph.edges.length).to.equal(1);
     });
   });
+
+  describe('to dot format', () => {
+    it('Check dot format', async () => {
+      await graph.init(oneObjectRecords);
+
+      expect(graph.toDotFormat()).to.contain(dotOutput1);
+    });
+
+  });
+
+  describe('getParentRecords', () => {
+    it('gets parent records of no parents', async () => {
+      await graph.init(oneObjectRecords);
+      
+      expect(graph.getParentRecords()).to.deep.equal(new Map());
+      expect(graph.nodes.length).to.equal(1);
+      expect(graph.edges.length).to.equal(1);
+    });
+  });
+
+  describe('toJson', () => {
+    it('check Json of 1 node 1 edge tree', async () => {
+      await graph.init(oneObjectRecords);
+      let key = '1';
+      let value = {parent: '', name: 'Object1', type: 'Object'};
+      let edge1 = {from: '1', to: '1'};
+
+      let expectedOutputNodes = new Array();
+      expectedOutputNodes.push({id: key, node: value});
+      expect(graph.toJson()).to.deep.equal({nodes: expectedOutputNodes, edges: [edge1]});
+      expect(graph.nodes.length).to.equal(1);
+      expect(graph.edges.length).to.equal(1);
+    });
+  });
+
+
 });
+
+describe('one relationship, two custom fields, one vrule dependency graph', () => {
+  let graph: DependencyGraph;
+ 
+  beforeEach(() => {
+    // Don't use a connection, just mock methods
+    graph = new DependencyGraph(null);
+
+
+    // Don't try to call out to tooling
+    stub(graph, 'retrieveRecords').returns(Promise.resolve([]));
+    stub(graph, 'retrieveCustomFields').returns(Promise.resolve(customFieldRecords));
+    stub(graph, 'retrieveValidationRules').returns(Promise.resolve(validationRuleRecords));
+    stub(graph, "retrieveCustomObjects").returns(Promise.resolve(customObjects));
+  });
+
+  // TODO you could have one of these for each public method in dependency graph.
+  // If you want to test some of the private methods, just make them public, otherwise
+  // they should be covered from the public method tests.
+  describe('init', () => {
+    it('populates nodes and edges for one record', async () => {
+      await graph.init(TwoFields1VRRecords);
+
+      expect(graph.nodes.length).to.equal(3);
+      expect(graph.edges.length).to.equal(2);
+    });
+  });
+
+  describe('getParentRecords', () => {
+    it('gets parent records of no parents', async () => {
+      await graph.init(TwoFields1VRRecords);
+      let expectedMap = new Map();
+      expectedMap.set('1', 'ObjectA');
+      expectedMap.set('2', 'ObjectA');
+      expectedMap.set('3', 'CustomObjectA');
+      expect(graph.getParentRecords()).to.deep.equal(expectedMap);
+    });
+  });
+
+  describe('to dot format', () => {
+    it('Check dot format', async () => {
+      await graph.init(TwoFields1VRRecords);
+
+      expect(graph.toDotFormat()).to.contain(dotOutput2);
+    });
+
+  });
+
+  
+});
+
